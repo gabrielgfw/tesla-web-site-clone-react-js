06b445c63152e463073df777c510eeff9ebcb4bf
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-02-15 15:01:23 -0300

    add colors and using map to render multiples Models

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import DefaultOverlayComponent from '../DefaultOverlayComponent';
+import { ModelSection, ModelsWrapper } from '../Model';
 
-import { Container } from './styles';
+import {Container} from './styles';
 
-const Page: React.FC = () => {
-  return (
-    <Container>
-      <h1>Page</h1>
-    </Container>
-  );
+const Page : React.FC = () => {
+    return (
+      <Container>
+        <ModelsWrapper>
+          <div>
+            {[
+              'Model One',
+              'Model Two',
+              'Model Three',
+              'Model Four',
+              'Model Five',
+              'Model Six',
+              'Model Seven',
+            ].map(modelName => (
+              <ModelSection
+                key={modelName}
+                className="colored"
+                modelName={modelName}
+                overlayNode= {
+                  <DefaultOverlayComponent
+                    label = {modelName}
+                    description = "Order Online for Delivery"
+                  />
+                }
+              />
+            ))}
+          </div> 
+        </ModelsWrapper>
+      </Container>
+    );
 };
 
 export default Page;
